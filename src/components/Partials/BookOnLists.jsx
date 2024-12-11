812f451e25c893603c74dd8a196186b1c70a4556
Ember Doyle <doyle.ember@gmail.com>
2024-12-11 12:12:29 -0700

    Add option to remove from specific list on book page

--- src/components/Partials/BookOnLists.jsx
+++ src/components/Partials/BookOnLists.jsx
@@ -20,12 +20,40 @@ export function BookOnLists({book}){
         let list = (lists.filter((listSearch)=>listSearch.id == listID))[0]
         navigate('../myBooks',{state:{list}});
     }
+    async function handleRemoveFromList(e){
+        let listID = e.target.id;
+
+        const response = await fetch(import.meta.env.VITE_BACKEND+"/lists/"+listID+"/deleteBook/"+book.book.id, {
+            method: "PUT",
+            mode:"cors",
+            headers: {
+              "Content-Type": "application/json",
+              "authorization": "Bearer " +token
+            },
+        }); 
+        if(response.status == 403){//if token is expired - log out and nav to login
+            localStorage.removeItem("token");
+            setToken(null);
+            navigate('../login');
+        }
+        if(response.status != 200 && response.status != 403){//if theres errors
+            const errors = await response.json();
+            console.log(errors)
+        }
+        else{
+            setEdit(!edit);
+        }
+    }
 
     return(
         <ul>
             {listsFound.map((list)=>{
                 return(
-                    <li  key ={list.id} id={list.id} style={{cursor:"grab"}} onClick={handleListNavigation}>{list.name} </li>
+                    <li  key ={list.id} >
+                        <div className="listName" id={list.id} style={{cursor:"grab"}} onClick={handleListNavigation}>{list.name} </div>
+                        <button id={list.id} onClick={handleRemoveFromList}>Remove from list</button>
+
+                    </li>
                 )
             })}
         </ul>
