f924556ede09571e50785f946874fc0a214d624d
Ember Doyle <doyle.ember@gmail.com>
2024-12-11 13:09:20 -0700

    Only show 5 feed items, and add a show more button

--- src/components/Partials/Feed/Feed.jsx
+++ src/components/Partials/Feed/Feed.jsx
@@ -1,10 +1,16 @@
 import { useOutletContext} from "react-router-dom";
 import FeedBook from "./FeedBook";
+import { useState } from "react";
+import { func } from "prop-types";
 
 export default function Feed() {
     const [token,setToken,edit,setEdit,lists,setLists] = useOutletContext();
+    const [count, setCount] = useState(5)
     if(lists == null){return}
-
+    function handleShowMore(){
+        let newCount = count +5;
+        setCount(newCount)
+    }
     const allEvents=[];
     lists.map((list) =>{
         list.books.map((book)=>{
@@ -16,8 +22,23 @@ export default function Feed() {
     });
     //order by date
     const sortedEvents = allEvents.sort((a, b) => new Date(a.assignedAt)- new Date(b.assignedAt));
-    return (
+    if(sortedEvents.length>sortedEvents.slice(-count).length) return (
        <div className="feed">
+           <div className="title">My Feed</div>
+           <ul>
+                {sortedEvents.slice(-count).map((book) => {
+                    return(
+                        <li key={crypto.randomUUID()} >
+                            <FeedBook book={book}/>
+                        </li>
+                    )
+                })}
+            </ul>
+            <button onClick={handleShowMore}>Show more</button>
+       </div>
+    )
+    return(
+        <div className="feed">
            <div className="title">My Feed</div>
            <ul>
                 {sortedEvents.map((book) => {
@@ -28,6 +49,7 @@ export default function Feed() {
                     )
                 })}
             </ul>
+            <div>End of feed</div>
        </div>
     )
 }
